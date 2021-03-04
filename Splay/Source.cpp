ebdf474aca08d37aad35214d5989525d51b627b9
Jesse Wood <jw209@uakron.edu>
2021-03-04 00:13:39 -0500

    added timer to source for testing runtimes

--- Splay/Source.cpp
+++ Splay/Source.cpp
@@ -4,17 +4,28 @@
 
 #include "Splay.h"
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 
 int main()
 {
+
+    // record start time
+    auto start = chrono::high_resolution_clock::now();
+
     Splay x;
     for (int i = 0; i < 1000000; i++) {
         x.insert(i);
     }
 
-    cout << x.isFound(-1042) << endl;
+    cout << x.isFound(100000) << endl;
+
+    // record end time
+    auto finish = chrono::high_resolution_clock::now();
+
+    chrono::duration<double> elapsed = finish - start;
+    cout << "Elapsed time: " << elapsed.count() << " s\n";
 
     return 0;
 }
